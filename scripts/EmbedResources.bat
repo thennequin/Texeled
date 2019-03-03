3a08d9a5425c0a342d0423d7a9a010080a535c74
Thibault Hennequin <cameleonth@gmail.com>
2019-03-03 21:41:55 +0100

    Update script for resource embedder

--- scripts/EmbedResources.bat
+++ scripts/EmbedResources.bat
@@ -1,3 +1,10 @@
 @echo off
-"%~dp0\ResourceEmbedder.exe" "%~dp0..\Resources" "%~dp0..\src\Resources"
+
+if not exist "ResourceEmbedder_x64.exe" (
+	echo Downloading ResourceEmbedder...
+	powershell [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri "https://github.com/thennequin/UtilsCollection/releases/download/Build1/ResourceEmbedder_x64.exe" -OutFile '%~dp0\ResourceEmbedder_x64.exe'
+)
+
+"%~dp0\ResourceEmbedder_x64.exe" "%~dp0..\Resources" "%~dp0..\src\Resources"
 echo "Finish"
+pause
