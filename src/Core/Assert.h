66582391805ab7e3552966b32b53523cc8ca821c
Thibault Hennequin <cameleonth@gmail.com>
2019-03-06 23:44:45 +0100

    Add CORE_VERIFY_OK for testing condition is ErrorCode::Ok

--- src/Core/Assert.h
+++ src/Core/Assert.h
@@ -17,5 +17,6 @@ namespace Core
 #endif //_DEBUG
 
 #define CORE_VERIFY(bCondition, ...) { Core::Assert((bCondition), CORE_ASSERT_STRINGIFY(bCondition), __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__, ""); }
+#define CORE_VERIFY_OK(bCondition, ...) { Core::Assert((bCondition) == ErrorCode::Ok, CORE_ASSERT_STRINGIFY(bCondition), __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__, ""); }
 
 #endif //__CORE_ASSERT_H__
