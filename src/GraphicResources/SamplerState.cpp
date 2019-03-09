d2549f905341ae08f2fd194e70f8472b474e7237
Thibault Hennequin <cameleonth@gmail.com>
2019-03-09 16:59:42 +0100

    Add SamplerState

---
+++ src/GraphicResources/SamplerState.cpp
@@ -0,0 +1,76 @@
+#include "GraphicResources/SamplerState.h"
+
+#include "Program.h"
+
+#include <D3D11.h>
+
+namespace GraphicResources
+{
+	D3D11_FILTER ConvertFilter(SamplerFilterEnum eFilter)
+	{
+		switch (eFilter)
+		{
+		case SamplerFilter::NEAREST:
+			return D3D11_FILTER_MIN_MAG_MIP_POINT;
+		case SamplerFilter::LINEAR:
+			return D3D11_FILTER_MIN_LINEAR_MAG_MIP_POINT;
+		case SamplerFilter::NEAREST_MIPMAP_NEAREST:
+			return D3D11_FILTER_MIN_MAG_MIP_POINT;
+		case SamplerFilter::NEAREST_MIPMAP_LINEAR:
+			return D3D11_FILTER_MIN_MAG_POINT_MIP_LINEAR;
+		case SamplerFilter::LINEAR_MIPMAP_NEAREST:
+			return D3D11_FILTER_MIN_LINEAR_MAG_MIP_POINT;
+		case SamplerFilter::LINEAR_MIPMAP_LINEAR:
+			return D3D11_FILTER_MIN_LINEAR_MAG_POINT_MIP_LINEAR;
+		}
+	}
+
+
+	D3D11_TEXTURE_ADDRESS_MODE ConvertWrap(WrapEnum eWrap)
+	{
+		switch (eWrap)
+		{
+		case Wrap::REPEAT:
+			return D3D11_TEXTURE_ADDRESS_WRAP;
+		case Wrap::CLAMP_TO_EDGE:
+			return D3D11_TEXTURE_ADDRESS_CLAMP;
+		case Wrap::MIRRORED_REPEAT:
+			return D3D11_TEXTURE_ADDRESS_MIRROR;
+		}
+	}
+
+	SamplerState::SamplerState()
+		: m_pSamplerState(NULL)
+	{
+	}
+
+	SamplerState::~SamplerState()
+	{
+		if (m_pSamplerState != NULL)
+			m_pSamplerState->Release();
+	}
+
+	ErrorCode SamplerState::Create(const Desc& oDesc)
+	{
+		if (m_pSamplerState != NULL)
+			m_pSamplerState->Release();
+
+		// Create texture sampler
+		D3D11_SAMPLER_DESC oSamplerDesc;
+		ZeroMemory(&oSamplerDesc, sizeof(oSamplerDesc));
+		oSamplerDesc.Filter = ConvertFilter(oDesc.eFilter);
+		oSamplerDesc.AddressU = ConvertWrap(oDesc.eWrapU);
+		oSamplerDesc.AddressV = ConvertWrap(oDesc.eWrapV);
+		oSamplerDesc.AddressW = ConvertWrap(oDesc.eWrapW);
+		oSamplerDesc.MipLODBias = 0.f;
+		oSamplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+		oSamplerDesc.MinLOD = 0.f;
+		oSamplerDesc.MaxLOD = 0.f;
+		if (Program::GetInstance()->GetDX11Device()->CreateSamplerState(&oSamplerDesc, &m_pSamplerState) == S_OK)
+		{
+			return ErrorCode::Ok;
+		}
+
+		return ErrorCode::Fail;
+	}
+}
---
+++ src/GraphicResources/SamplerState.h
@@ -0,0 +1,58 @@
+#ifndef __GRAPHIC_RESOURCE_SAMPLER_STATE_H__
+#define __GRAPHIC_RESOURCE_SAMPLER_STATE_H__
+
+#include "Core/ErrorCode.h"
+
+struct ID3D11SamplerState;
+
+namespace GraphicResources
+{
+	struct SamplerFilter
+	{
+		enum Enum
+		{
+			NEAREST,
+			LINEAR,
+			NEAREST_MIPMAP_NEAREST,
+			NEAREST_MIPMAP_LINEAR,
+			LINEAR_MIPMAP_NEAREST,
+			LINEAR_MIPMAP_LINEAR
+		};
+	};
+	typedef SamplerFilter::Enum SamplerFilterEnum;
+
+	struct Wrap
+	{
+		enum Enum
+		{
+			REPEAT,
+			CLAMP_TO_EDGE,
+			MIRRORED_REPEAT,
+		};
+	};
+	typedef Wrap::Enum WrapEnum;
+
+	class SamplerState
+	{
+	public:
+		typedef struct
+		{
+			SamplerFilterEnum	eFilter;
+			WrapEnum			eWrapU;
+			WrapEnum			eWrapV;
+			WrapEnum			eWrapW;
+		}Desc;
+	public:
+		SamplerState();
+		~SamplerState();
+
+		ErrorCode				Create(const Desc& oDesc);
+
+		ID3D11SamplerState*		GetResource() { return m_pSamplerState; }
+	protected:
+		ID3D11SamplerState*		m_pSamplerState;
+	};
+}
+//namespace GraphicResources
+
+#endif // __GRAPHIC_RESOURCE_SAMPLER_STATE_H__
