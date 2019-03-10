7d00a86f2b41c118037c5389bb86a83b23dc424b
Thibault Hennequin <cameleonth@gmail.com>
2019-03-10 17:41:07 +0100

    Display console in Debug

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,12 @@ int main(int argc, char* argv[])
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc(1337);
 
+#ifdef _DEBUG
+	AllocConsole();
+	AttachConsole(GetCurrentProcessId());
+	freopen("CON", "w", stdout);
+#endif //_DEBUG
+
 	Program* pInstance = Program::CreateInstance(argc, argv);
 
 	while (pInstance->Run()) Sleep(8);
