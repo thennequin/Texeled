f23869db9021806497d58a5d7a6b23766ab8696b
Thibault Hennequin <cameleonth@gmail.com>
2019-03-15 14:43:05 +0100

    Display modes in toolbar

--- src/Toolbar.cpp
+++ src/Toolbar.cpp
@@ -16,5 +16,18 @@ Toolbar::~Toolbar()
 
 void Toolbar::OnToolBar()
 {
-	
+	ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 15.f);
+	ProgramModeEnum eMode = Program::GetInstance()->GetMode();
+	for (int iMode = 0; iMode < ProgramMode::_COUNT; ++iMode)
+	{
+		if (iMode > 0)
+			ImGui::SameLine(0,0);
+
+		ImGuiUtils::GroupedPartEnum eToolbarMode = ( iMode == 0 ) ? ImGuiUtils::GroupedPartEnum::LEFT : ( iMode == ProgramMode::_LAST ) ? ImGuiUtils::GroupedPartEnum::RIGHT : ImGuiUtils::GroupedPartEnum::MIDDLE;
+		if (ImGuiUtils::GroupedButton(ProgramModeStrings[iMode], iMode == eMode, eToolbarMode ))
+		{
+			Program::GetInstance()->SetMode((ProgramModeEnum)iMode);
+		}
+	}
+	ImGui::PopStyleVar();
 }
