f32ff8b9f15902111397eef799f94078a99d6461
Thibault Hennequin <cameleonth@gmail.com>
2020-03-29 18:34:58 +0200

    Rename Math.h to Arithmetic.h and add Math/Constants.h

---
+++ src/Math/Constants.h
@@ -0,0 +1,11 @@
+#ifndef __MATH_CONSTANTS_H__
+#define __MATH_CONSTANTS_H__
+
+namespace Math
+{
+	const double c_fPi = 3.14159265358979323846;
+	const double c_fInvPi = 0.31830988618379067153;
+	const double c_f2Pi = 6.28318530717958647692;
+}
+
+#endif //__MATH_CONSTANTS_H__
