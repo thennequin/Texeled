cd2715b53e43fa0fa07d18c38be7769ae9af1c9f
Thibault Hennequin <cameleonth@gmail.com>
2020-04-12 22:19:16 +0200

    Fix re-download of ResourceEmbedder_x64.exe in EmbedResources.bat

--- scripts/EmbedResources.bat
+++ scripts/EmbedResources.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if not exist "ResourceEmbedder_x64.exe" (
+if not exist "%~dp0\ResourceEmbedder_x64.exe" (
 	echo Downloading ResourceEmbedder...
 	powershell [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri "https://github.com/thennequin/UtilsCollection/releases/download/Build1/ResourceEmbedder_x64.exe" -OutFile '%~dp0\ResourceEmbedder_x64.exe'
 )
