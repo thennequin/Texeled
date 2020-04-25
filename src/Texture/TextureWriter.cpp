fd2885c682ad5f0e3146813d33204343213daffc
Thibault Hennequin <cameleonth@gmail.com>
2020-04-26 00:25:13 +0200

    Add custom settings system to TextureWriter

--- src/Texture/TextureWriter.cpp
+++ src/Texture/TextureWriter.cpp
@@ -7,24 +7,79 @@
 namespace Texture
 {
 	static Core::Array<TextureWriterInfo>	s_oTextureWriters;
+	static bool s_bRegisteringTextureWriter = false;
 
-	TextureWriterHandle RegisterTextureWriter(const char* pName, const char* pExts, TextureWriterFunc pWriter, TextureWriterSupportedFunc pWriterTester)
+	TextureWriterHandle BeginRegisterTextureWriter(const char* pName, const char* pExts, TextureWriterFunc pWriter, TextureWriterSupportedFunc pWriterTester, size_t iSizeofStruct)
 	{
 		CORE_ASSERT(pName != NULL);
 		CORE_ASSERT(pExts != NULL);
 		CORE_ASSERT(pWriter != NULL);
 		CORE_ASSERT(pWriterTester != NULL);
+		CORE_ASSERT(s_bRegisteringTextureWriter == false);
 
-		if (pName == NULL || pExts == NULL || pWriter == NULL || pWriterTester == NULL)
+		if (pName == NULL || pExts == NULL || pWriter == NULL || pWriterTester == NULL || s_bRegisteringTextureWriter)
 			return TextureWriterHandleInvalid;
+
+		s_bRegisteringTextureWriter = true;
 
 		TextureWriterInfo oInfo;
 		oInfo.pName = pName;
 		oInfo.pExt = pExts;
 		oInfo.pWriter = pWriter;
 		oInfo.pTester = pWriterTester;
+		oInfo.iSettingsStructSize = iSizeofStruct;
 		s_oTextureWriters.push_back(oInfo);
 		return s_oTextureWriters.size() - 1;
+	}
+
+	void AddSettingBool(const char* pName, intptr_t iOffset, bool bDefault)
+	{
+		CORE_ASSERT(s_bRegisteringTextureWriter == true);
+		s_oTextureWriters.back().oSettings.push_back(WriterSetting());
+		WriterSetting& oSetting = s_oTextureWriters.back().oSettings.back();
+		oSetting.sName = pName;
+		oSetting.iOffset = iOffset;
+		oSetting.eType = SettingType::BOOL;
+		oSetting.oDefault.Bool = bDefault;
+	}
+
+	void AddSettingFloat(const char* pName, intptr_t iOffset, float fDefault, float fMin, float fMax)
+	{
+		CORE_ASSERT(s_bRegisteringTextureWriter == true);
+		s_oTextureWriters.back().oSettings.push_back(WriterSetting());
+		WriterSetting& oSetting = s_oTextureWriters.back().oSettings.back();
+		oSetting.sName = pName;
+		oSetting.iOffset = iOffset;
+		oSetting.eType = SettingType::FLOAT;
+		oSetting.oDefault.Float = fDefault;
+		oSetting.oMin.Float = fMin;
+		oSetting.oMax.Float = fMax;
+	}
+
+	void AddSettingInteger(const char* pName, intptr_t iOffset, int iDefault, int iMin, int iMax)
+	{
+		CORE_ASSERT(s_bRegisteringTextureWriter == true);
+		s_oTextureWriters.back().oSettings.push_back(WriterSetting());
+		WriterSetting& oSetting = s_oTextureWriters.back().oSettings.back();
+		oSetting.sName = pName;
+		oSetting.iOffset = iOffset;
+		oSetting.eType = SettingType::INTEGER;
+		oSetting.oDefault.Integer = iDefault;
+		oSetting.oMin.Integer = iMin;
+		oSetting.oMax.Integer = iMax;
+	}
+
+	void EndRegisterTextureWriter()
+	{
+		CORE_ASSERT(s_bRegisteringTextureWriter == true);
+		s_bRegisteringTextureWriter = false;
+	}
+
+	TextureWriterHandle RegisterTextureWriter(const char* pName, const char* pExts, TextureWriterFunc pWriter, TextureWriterSupportedFunc pWriterTester)
+	{
+		TextureWriterHandle hWriter = BeginRegisterTextureWriter(pName, pExts, pWriter, pWriterTester, 0);
+		EndRegisterTextureWriter();
+		return hWriter;
 	}
 
 	const TextureWriterInfo* GetTextureWriter(TextureWriterHandle hWriter)
@@ -37,7 +92,37 @@ namespace Texture
 		return &s_oTextureWriters[hWriter];
 	}
 
-	ErrorCode SaveToStream(Graphics::Texture* pTexture, const WriterSettings* pSettings, IO::Stream* pStream, const char* pFilename, const TextureWriterInfo* pUseWriter)
+	void* CreateSettingsStruct(const TextureWriterInfo* pWriterInfo)
+	{
+		if (pWriterInfo->iSettingsStructSize == 0)
+			return NULL;
+
+		char* pSettings = (char*)malloc(pWriterInfo->iSettingsStructSize);
+		if (pSettings == NULL)
+			return NULL;
+
+		for (size_t iSettingIndex = 0; iSettingIndex < pWriterInfo->oSettings.size(); ++iSettingIndex)
+		{
+			const WriterSetting& oSettingInfo = pWriterInfo->oSettings[iSettingIndex];
+			void* pSetting = pSettings + oSettingInfo.iOffset;
+			switch (oSettingInfo.eType)
+			{
+			case SettingType::BOOL:
+				*(bool*)pSetting = oSettingInfo.oDefault.Bool;
+				break;
+			case SettingType::FLOAT:
+				*(float*)pSetting = oSettingInfo.oDefault.Float;
+				break;
+			case SettingType::INTEGER:
+				*(int*)pSetting = oSettingInfo.oDefault.Integer;
+				break;
+			}
+		}
+
+		return pSettings;
+	}
+
+	ErrorCode SaveToStream(Graphics::Texture* pTexture, const void* pSettings, IO::Stream* pStream, const char* pFilename, const TextureWriterInfo* pUseWriter)
 	{
 		if (pTexture != NULL)
 		{
@@ -75,7 +160,7 @@ namespace Texture
 		return ErrorCode(1, "Invalid arguments");
 	}
 
-	ErrorCode SaveToFile(Graphics::Texture* pTexture, const WriterSettings* pSettings, const char* pFilename, const TextureWriterInfo* pUseWriter)
+	ErrorCode SaveToFile(Graphics::Texture* pTexture, const void* pSettings, const char* pFilename, const TextureWriterInfo* pUseWriter)
 	{
 		if (pTexture != NULL)
 		{
--- src/Texture/TextureWriter.h
+++ src/Texture/TextureWriter.h
@@ -1,15 +1,13 @@
 #ifndef __TEXTURE_TEXTURE_WRITER_H__
 #define __TEXTURE_TEXTURE_WRITER_H__
 
+#include "Core/Array.h"
+#include "Core/String.h"
+
 #include "Graphics/Texture.h"
 
 namespace Texture
 {
-	struct WriterSettings
-	{
-		unsigned char							iQuality; // 0-100
-	};
-
 	enum ESupportedWriter
 	{
 		E_SUPPORTED_WRITER_FALSE,
@@ -17,8 +15,45 @@ namespace Texture
 		E_SUPPORTED_WRITER_FULL,
 	};
 
+	struct _SettingType
+	{
+		enum Enum
+		{
+			BOOL,
+			FLOAT,
+			INTEGER
+		};
+	};
+	typedef _SettingType::Enum SettingType;
+
 	typedef ESupportedWriter(*TextureWriterSupportedFunc)(Graphics::Texture* pTexture);
-	typedef bool(*TextureWriterFunc)(Graphics::Texture* pTexture, const WriterSettings* pSettings, IO::Stream* pStream);
+	typedef bool(*TextureWriterFunc)(Graphics::Texture* pTexture, const void* pSettings, IO::Stream* pStream);
+
+	typedef struct
+	{
+		Core::String				sName;
+		intptr_t					iOffset;
+		SettingType					eType;
+		union
+		{
+			bool					Bool;
+			float					Float;
+			int						Integer;
+		} oDefault;
+
+		union
+		{
+			float					Float;
+			int						Integer;
+		} oMin;
+
+		union
+		{
+			float					Float;
+			int						Integer;
+		} oMax;
+	} WriterSetting;
+
 
 	typedef struct
 	{
@@ -26,17 +61,31 @@ namespace Texture
 		const char*					pExt;
 		TextureWriterFunc			pWriter;
 		TextureWriterSupportedFunc	pTester;
+		size_t						iSettingsStructSize;
+		Core::Array<WriterSetting>	oSettings;
 	} TextureWriterInfo;
 
 	typedef size_t TextureWriterHandle;
 	const TextureWriterHandle TextureWriterHandleInvalid = (TextureWriterHandle)-1;
 
+#define STRUCT_MEMBER_OFFSET(Struct, Member) ((intptr_t)(&((Struct*)0)->Member))
+
+	/* */
+	TextureWriterHandle				BeginRegisterTextureWriter(const char* pName, const char* pExts, TextureWriterFunc pWriter, TextureWriterSupportedFunc pWriterTester, size_t iSizeofStruct);
+	void							AddSettingBool(const char* pName, intptr_t iOffset, bool bDefault);
+	void							AddSettingFloat(const char* pName, intptr_t iOffset, float fDefault, float fMin, float fMax);
+	void							AddSettingInteger(const char* pName, intptr_t iOffset, int iDefault, int iMin, int iMax);
+	void							EndRegisterTextureWriter();
+
+	// Simple writer without settings
 	TextureWriterHandle				RegisterTextureWriter(const char* pName, const char* pExts, TextureWriterFunc pWriter, TextureWriterSupportedFunc pWriterTester);
 
 	const TextureWriterInfo*		GetTextureWriter(TextureWriterHandle hWriter);
 
-	ErrorCode						SaveToStream(Graphics::Texture* pTexture, const WriterSettings* pSettings, IO::Stream* pStream, const char* pFilename, const TextureWriterInfo* pUseWriter = NULL);
-	ErrorCode						SaveToFile(Graphics::Texture* pTexture, const WriterSettings* pSettings, const char* pFilename, const TextureWriterInfo* pUseWriter = NULL);
+	void*							CreateSettingsStruct(const TextureWriterInfo* pWriterInfo);
+
+	ErrorCode						SaveToStream(Graphics::Texture* pTexture, const void* pSettings, IO::Stream* pStream, const char* pFilename, const TextureWriterInfo* pUseWriter = NULL);
+	ErrorCode						SaveToFile(Graphics::Texture* pTexture, const void* pSettings, const char* pFilename, const TextureWriterInfo* pUseWriter = NULL);
 	void							GetTextureWriters(const TextureWriterInfo** pOutWriters, int* pOutCount);
 }
 //namspace Texture
