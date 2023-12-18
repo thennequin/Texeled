df7fb29855b5a7d40570fe9295c009435f8370fd
Thibault Hennequin <cameleonth@gmail.com>
2023-12-18 16:34:16 +0100

    Use sqrtf from math.h in Math::Sqrt

--- src/Math/Arithmetic.cpp
+++ src/Math/Arithmetic.cpp
@@ -1,16 +1,20 @@
 #include "Math/Arithmetic.h"
 
+#include <math.h> // sqrtf
+
 namespace Math
 {
 	float Sqrt(float fValue)
 	{
-		union { int iX; float fX; };
+		return sqrtf(fValue);
+
+		/*union { int iX; float fX; };
 		fX = fValue;
 		float fXHalf = 0.5f * fX;
 		iX = 0x5F37599E - (iX >> 1);
 		fX = fX * (1.5f - fXHalf * fX * fX);
 		fX = fX * (1.5f - fXHalf * fX * fX);
-		return fX;
+		return fX;*/
 	}
 
 	uint8_t HighBitFirst(uint64_t iNumber)
